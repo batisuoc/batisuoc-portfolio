573bf8f0d114cec5b2b4a63df11fc2858070cd8f
batisuoc <zhenghengyue@gmail.com>
2019-09-20 21:48:06 +0700

    Done slideshow

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 var emailValue = document.getElementById("email");
 var messValue = document.getElementById("message");
 var noEmailNotice = document.getElementById("no-email-notice");
@@ -44,4 +46,24 @@ function checkValid() {
     invalidEmail.style.display = "block";
     event.preventDefault();
   }
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
